c3efc1f1109c744b0ef9226da7410efd633dae07
moayed-mustafa <moayed.abrida@yahoo.com>
2020-03-22 19:35:07 +0400

    changing between players and alerting a tie message

--- connect4.js
+++ connect4.js
@@ -11,12 +11,13 @@ let HEIGHT = 6;
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
+
+
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
   // should add p1 || p2 in this  array
   board = Array.from({ length: HEIGHT }, () => Array.from({ length: WIDTH }, () => null));
 }
@@ -25,10 +26,8 @@ function makeBoard() {
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
   const htmlBoard = document.querySelector('#board');
 
-  // TODO: making the event-based table rows
   let top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
@@ -40,7 +39,6 @@ function makeHtmlBoard() {
   }
   htmlBoard.append(top);
 
-  // TODO: making the  game rows and data
   for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
     for (let x = 0; x < WIDTH; x++) {
@@ -55,22 +53,24 @@ function makeHtmlBoard() {
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-
-  console.log('x from the findspot for colum function', x);
   for (let i = board.length - 1; i >= 0; i--){
     if (board[i][x] === null) { return i }
   }
+    return null
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
-  const tdPiece = document.createElement('div');
-  tdPiece.classList.add('piece');
-  tdPiece.classList.add('p1');
+  // the variable tdPiece might need to be a global one in the future
+  let tdPiece = document.createElement('div');
+  tdPiece.classList.add('piece','p1','p2');
+
+  // check what the value of currentPlayer
+  currPlayer===1? tdPiece.classList.toggle('p2'):tdPiece.classList.toggle('p1') ;
   let td = document.getElementById(`${y}-${x}`)
-  td.append(tdPiece);
+  td.append(tdPiece)
 
 }
 
@@ -78,6 +78,7 @@ function placeInTable(y, x) {
 
 function endGame(msg) {
   // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -88,18 +89,14 @@ function handleClick(evt) {
   let x = +evt.target.id;
   // get next spot in column (if none, ignore click)
   let y = findSpotForCol(x);
-  console.log(x,y)
   if (y === null) {
     return;
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
-  // should update the corresponding board column/row.
+  currPlayer===1? board[y][x] = 'red' : board[y][x] = 'blue';
   placeInTable(y, x);
-  // the cell should be filled with the player who filled the board
-  board[y][x] = 'red';
-  console.log(board);
+  // console.log(board);
 
 
   // check for win
@@ -108,10 +105,13 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
+  if (checkForTie() ) {
+    return endGame(`It's a tie`);
+  }
 
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  currPlayer === 1 ? currPlayer = 2 : currPlayer = 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -146,6 +146,9 @@ function checkForWin() {
       }
     }
   }
+}
+function checkForTie(){
+  for (let row of board) { return row.every(data => data !== null); }
 }
 
 makeBoard();
